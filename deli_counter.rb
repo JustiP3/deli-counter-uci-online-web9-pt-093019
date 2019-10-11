7369778282d056172af1961bc625134fbb9d51af
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 02:39:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,26 +6,25 @@ end
 
 def line(deli)
   
-if deli == []
+  # if line is empty return empty message and return from method 
+if deli == [] 
   puts "The line is currently empty."
   return 
 end
 
+#if line is not empty create a line message then print that message 
+
   message = "The line is currently: "
-  puts message 
   position = 1 
   index = 0 
   while index < deli.length  do 
     name = deli[index]
-    message = "#{position}. #{name} "
-    puts message
+    message << "#{position}. #{name} "
     position += 1 
     index += 1 
- 
- 
 end
 
-
+puts message 
 
 end
 
